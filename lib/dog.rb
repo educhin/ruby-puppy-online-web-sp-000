ffb9479cc1b4549b07ab2924d5f7ddc6600b66aa
Chinedu Arinze <c.s.arinze@gmail.com>
2019-04-12 04:49:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,13 +5,13 @@ class Dog
   attr_accessor :name
 
   def initialize(name)
-    @@name = name
+    @name = name
     @@all << self
   end
 
   def self.all
     @@all.each do |dog|
-      puts dog
+      puts dog.name
     end
   end
 
